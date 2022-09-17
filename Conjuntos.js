92fafad2c496f443f82b69038a8e29d30a707ba5
Ivaan3 <106582115+Ivaan3@users.noreply.github.com>
2022-09-17 08:22:06 +0200

    Add files via upload

---
+++ Conjuntos.js
@@ -0,0 +1,11 @@
+const familia = ["Joel", "Marcelo", "Graciela", "Ivan"];
+
+const set_familia = new Set(familia);
+console.log(set_familia);
+
+set_familia.add("Ivan");
+console.log(set_familia);
+
+set_familia.add("JavaScript")
+
+console.log(set_familia);
